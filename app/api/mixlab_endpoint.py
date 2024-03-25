04c50c55ce00f08d3fbce1e64093648c5e3b8f26
yansenessa <yang82101@163.com>
2024-03-26 01:01:45 +0800

    oss

--- app/api/mixlab_endpoint.py
+++ app/api/mixlab_endpoint.py
@@ -24,8 +24,8 @@ import oss2
 
 
 router = APIRouter()
-access_key_id = ''
-access_key_secret = ''
+access_key_id = 'LTAI5tBXTV9yiiqu2rsRjEyj'
+access_key_secret = 'EUwQQdLvTJQbhFwM5n4TyrAl6oN8WJ'
 endpoint = 'http://oss-cn-beijing.aliyuncs.com'
 # Dependency
 def get_db():
